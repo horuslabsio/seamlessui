17a91ac587b72e209c8aa904299d2973ab1efc65
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-10-09 17:50:01 +0100

    refactor: removed ` tooling/tailwind.json`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import tailwindConfig from "./tooling/tailwind.json";
 
 const config: Config = {
   content: [
@@ -7,7 +6,37 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  presets: [tailwindConfig],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "light-linear-gradient":
+          "linear-gradient(168.54deg, #FF9034 -46.81%, #FFFFFF 31.09%, #FFFFFF 77.47%)",
+        "dark-linear-gradient":
+          "linear-gradient(169.58deg, #E1852D -79.18%, #212121 19.19%, #1A1A1A 56.31%)",
+      },
+      fontFamily: {
+        "helvetica-extrabold": ["helvetica-extrabold", "sans-serif"],
+        "helvetica-bold": ["helvetica-bold", "sans-serif"],
+        "helvetica-medium": ["helvetica-medium", "sans-serif"],
+      },
+      colors: {
+        "base-light": "#ffffff",
+        "base-dark": "#1A1A1A",
+        "blue-700": "#141925",
+        "red-600": "#CC3333",
+        "grey-10": "#F5F5F5",
+        "grey-50": "#fafafa",
+        "grey-100": "#F7F7F7",
+        "grey-200": "#EEEEEE",
+        "grey-300": "#DADADA",
+        "grey-500": "#7A7A7A",
+        "grey-600": "#3A3A3A",
+        "grey-700": "#494949",
+        "grey-800": "#343434",
+        "grey-900": "#222222",
+      },
+    },
+  },
   plugins: [
     function ({ addUtilities }: { addUtilities: any }) {
       addUtilities({
@@ -21,4 +50,5 @@ const config: Config = {
     },
   ],
 };
+
 export default config;
